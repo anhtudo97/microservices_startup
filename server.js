9d227937fe0df021ee33ad8774621830b48222e6
unknown <anhtudo97@gmail.com>
2020-08-10 10:53:28 +0700

    feat: init

--- server.js
+++ server.js
@@ -1,14 +1,1 @@
 const server = require('express')();
-const { databaseFunction } = require('./database');
-
-server
-    .get('/', (req, res) => {
-        res.send(databaseFunction());
-    })
-    .get('/another', (_, res) => {
-        res.send('still working');
-    })
-
-server.listen(3000, () => {
-    console.log('I am listening on port 4000');
-})
